e2790bf9c5456d9ae31268358369586e017d32ac
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2020-03-30 09:27:00 -0300

    move LastUpdate to footer

--- src/components/container.js
+++ src/components/container.js
@@ -1,17 +1,14 @@
 import React from "react"
 import { Header } from "./header"
 import { Nav } from "./nav"
-import { LastUpdate } from "./last-update"
+
 import styles from "../styles/container.module.css"
 import { Footer } from "./footer"
 
 export const Container = ({ children }) => (
   <div className={styles.container}>
     <Header />
-    <div className={styles.navContainer}>
-      <Nav />
-      <LastUpdate />
-    </div>
+    <Nav />
     <div className={styles.content}>{children}</div>
     <Footer />
   </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,19 @@
 import React from "react"
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { LastUpdate } from "./last-update"
 import styles from "../styles/footer.module.css"
 
 export const Footer = () => (
-  <div className={styles.footer}>
-    <a href="https://github.com/luiz290788/covid19-brazil">
-      <FontAwesomeIcon size="lg" icon={faGithub} />
-    </a>
-    <a href="https://twitter.com/l2zg7e">
-      <FontAwesomeIcon size="lg" icon={faTwitter} />
-    </a>
-  </div>
+  <>
+    <LastUpdate />
+    <div className={styles.footer}>
+      <a href="https://github.com/luiz290788/covid19-brazil">
+        <FontAwesomeIcon size="lg" icon={faGithub} />
+      </a>
+      <a href="https://twitter.com/l2zg7e">
+        <FontAwesomeIcon size="lg" icon={faTwitter} />
+      </a>
+    </div>
+  </>
 )
