131b85121e195360651cfe3a8855eab394633861
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2020-07-19 20:21:23 -0300

    fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,33 +58,5 @@ const createRegionPages = async ({ graphql, actions }) => {
   )
 }
 
-const createMDNodes = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/static-page.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-      },
-    })
-  })
-}
-
 exports.createPages = options =>
-  Promise.all([createRegionPages, createMDNodes].map(f => f(options)))
+  Promise.all([createRegionPages].map(f => f(options)))
